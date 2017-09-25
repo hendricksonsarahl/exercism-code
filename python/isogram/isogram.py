2a9c0dafbf08b6e0110cd858ee6004bd4ed7b37a
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-09-25 13:09:06 -0700

    All tests passing, submitting isogram.py

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -2,7 +2,24 @@ def is_isogram(string):
     '''An isogram is a word which has no
     repeating letters'''
 
+    # Validate that input is of String type
+    if not isinstance(string, str):
+        return False
+
+    # If string is empty, no letters repeat so it passes
+    if len(string) == 0:
+        return True
+    
+    # Initialize variable
+    isogram = True
+
+    # Lowercase conversion to make case insensitive
+    string = string.lower()
+
+    # Ignore non-ascii characters to allow for spaces
+    # and special characters to repeat
+
     for char in string:
-        if string.count(char) > 1:
-            return False
-    return True
+        if string.count(char) > 1 and char.isalpha():
+            isogram = False
+    return isogram
