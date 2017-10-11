fc60c354dfbe0211838a453c7c53c3934e9ae129
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-10-10 19:49:25 -0700

    Submitted pangram.py exercise

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,2 +1,13 @@
 def is_pangram():
-    pass
+    '''A function to determine if a given sentence is
+    a pangram, a sentence using every letter of the 
+    alphabet at least once.'''
+
+    '''
+    Start with all letters of the alphabet. As you
+    loop over the test string 'string', remove a letter 
+    from the alpabet list as you come to it. If all get removed,
+    the string is a lambda.
+    '''
+
+is_pangram = lambda string: not set('abcdefghijklmnopqrstuvwxyz') - set(string.lower())
