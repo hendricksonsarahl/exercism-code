3ae49bec9e58f4e18521fdbe61a2e04f1452831a
Sarah Hendrickson <hendricksonsarahl@gmail.com>
2017-10-16 20:04:28 -0700

    Completed rna-transcription algorithm

--- python/rna-transcription/rna_transcription.py
+++ python/rna-transcription/rna_transcription.py
@@ -1,8 +1,20 @@
 def to_rna(dna):
-    
+    '''
+    Given a DNA strand, its transcribed RNA strand is formed
+    by replacing each nucleotide with its complement:
+    '''
+
     complement = {'G': 'C',
               'C': 'G',
               'T': 'A',
               'A': 'U'}
+
+    '''
+    Make sure imputs are valid DNA nucleotides 
+    '''
+
+    for x in dna:
+        if x not in ['G', 'C', 'T', 'A']:
+            return ''
 
     return ''.join(complement[nucleotide] for nucleotide in dna)
